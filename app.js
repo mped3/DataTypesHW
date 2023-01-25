c1ac33d40e6cf91009c2b420dc640ac4c7cf0b57
unknown <pedifordm@icloud.com>
2023-01-24 21:13:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,124 @@
+// Numbers:
+let b = 22;
+b.valueOf();
+(22).valueOf();
+(2+20).valueOf();
+console.log(.valueOf)
+
+
+let length = 20;
+console.log(length +3)
+
+let weight = 72;
+console.log(weight)
+
+let height = 55: 
+console.log(height)
+
+
+let x = 10;
+let y = 20;
+let z = x + y;
+console.log(z)
+
+let x = 2;
+console.log(x)
+
+let x = 10;
+console.log(x)
+
+let x = 178e5;
+console.log(x)
+
+let y = 3.15;
+console.log(y)
+
+let length = 3000943
+console.log(length)
+
+
+// Strings:
+let myFavSport = "boxing"
+console.log(myFavSport)
+
+let myFingers = "10"
+console.log(myFingers)
+
+let myOccupation = "swim instructor"
+console.log(myOccupation)
+
+let myFavColor = "blue"
+console.log(myFavColor)
+
+let myAge = "20"
+console.log(myAge)
+
+let mySchool = "PerScholas"
+console.log(mySchool)
+
+let color = "blue";
+console.log(color)
+
+let lastName = "pediford";
+console.log(lastName)
+
+let familyMembers = ["Kelly","Jackie","Kyle"]
+console.log(familyMembers)
+
+let fruits = ["apple","orange","cranberry"]
+fruits.toString();
+console.log(fruits)
+
+// Object:
+const person = {firstName:"Mia", lastName:"Pediford"};
+const birthday = {bDay:"OCT26"}
+const perScholasStudent = {
+    firstName:"Mia", lastName:"Pediford", age:"2o", bDay:"OCT26", enrolled:"YES", marks: "softwaredevelopment", driving: "yes", gpa:"3.2", courseLevel:"1", courseGrade:"90%"
+}
+console.log(perScholasStudent.lastName)
+console.log(perScholasStudent.firstName)
+console.log(perScholasStudent.age)
+
+
+// Array object:
+const cars = ["Benz", "AUDI", "BMW"]
+console.log(cars)
+
+let borough = ["harlem","bronx","brooklyn"]
+borough.shift("bronx")
+console.log(borough)
+
+let destinyChild =["Kelly"]
+destinyChild.push("Beyonce")
+console.log(destinyChild)
+
+const letters = ["A","B","C","D"]
+console.log(letters)
+
+let myFam = ["Gina","Martin","Pam"]
+console.log(myFam)
+
+let evenNum = [2,4,6,8]
+console.log(evenNum)
+
+const oddNum = [1,3,5,7]
+console.log(oddNum)
+
+let milkShake = ["milk","sugar","ice-cream","ice"]
+console.log(milkShake)
+
+let bestArtist = ["Nas","Fabolous","Stack Bunldes","Jadakiss"]
+console.log(bestArtist)
+
+let Grammy = ["nas","jay z","future"]
+console.log(Grammy)
+
+
+
+
+
+
+
+
+
+
